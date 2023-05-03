eb8b83a20411208f1fab13d1765ca38da9c1bdc1
InnaStepanova <inna-panichkina@yandex.ru>
2023-05-03 10:23:56 +0300

    Initial Commit

---
+++ Gradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gradient
+//
+//  Created by Лаванда on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
